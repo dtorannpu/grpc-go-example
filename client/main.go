6a2b82be64c2f1872c2771edafbc0bb8f6e4d51a
dtorannpu <dtorannpu@gmail.com>
2024-09-23 13:58:27 +0900

    クライアント作成

---
+++ client/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"github.com/dtorannpu/grpc-example/sample"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"time"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+)
+
+func main() {
+	flag.Parse()
+	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal().Err(err).Msg("did not connect")
+	}
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatal().Err(err).Msg("could not close connection")
+		}
+	}(conn)
+	c := sample.NewSampleServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Sample(ctx, &sample.SampleRequest{})
+	if err != nil {
+		log.Fatal().Err(err).Msg("could not sample request")
+	}
+	log.Printf("response %s", r.GetMessage())
+}
