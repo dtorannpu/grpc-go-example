ccad4c243b1004fc9f5a06897c3db01325926652
dtorannpu <dtorannpu@gmail.com>
2024-09-23 13:37:53 +0900

    gprcサーバー作成

---
+++ server/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+
+	"github.com/dtorannpu/grpc-example/sample"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct {
+	sample.UnimplementedSampleServiceServer
+}
+
+func (s *server) Sample(ctx context.Context, request *sample.SampleRequest) (*sample.SampleResponse, error) {
+	return &sample.SampleResponse{Message: "Sample"}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to listen")
+	}
+	s := grpc.NewServer()
+	sample.RegisterSampleServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatal().Err(err).Msg("failed to server")
+	}
+}
